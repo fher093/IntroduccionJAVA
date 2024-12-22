a3015a4e2cf9479da6edda61296fbe621699272a
Fernando <fherdavidlleru@gmail.com>
2024-12-22 10:12:41 -0500

    Manejo y Lectura de Archivos

--- src/ManejoDeArchivos/Archivo2.java
+++ src/ManejoDeArchivos/Archivo2.java
@@ -1,48 +1,80 @@
-
 package ManejoDeArchivos;
 
 import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Archivo2 {
+
     public static void main(String[] args) {
-        //CARGAR EL ARCHIVO E IMPRIMIR TODOS LOS POKEMON
-        //GUARDAR EN UNA LISTA DE CADENA LOS POKEMON LEGENDARIOS
+       //generacionLegendarios(); 
+       terminadoVocal();
+    } 
+//añadir en una lista los pokemones no legendarios de generacion 3
+    public static void generacionLegendarios(){
         try{
-            String ruta = "C:\\Users\\usuario\\Desktop\\Primero 24-25\\PROGRAMACION\\pokemon.csv";  
-            File archivo = new File(ruta); 
-            Scanner leer = new Scanner(archivo); 
-            ArrayList <String> legendarios = new ArrayList<>();
-            int contador = 0; 
-            //  MUEVE EL PUNTERO A LA SIGUIENTE POSICION EVITANDO LA CABECERA 
-            leer.hasNextLine();
+             
+           //enlace de lectura 
+            File archivo = new File("C:\\Users\\usuario\\Desktop\\Primero 24-25\\PROGRAMACION\\pokemon.csv");
+            File salida = new File("C:\\Users\\usuario\\Desktop\\Primero 24-25\\PROGRAMACION\\generacionNolegendaria.csv");
+            Scanner teclado = new Scanner(archivo); 
             
-            while (leer.hasNextLine()) {      
-                String cadena = leer.nextLine();  
-                String v[]= cadena.split(","); 
-                boolean b = Boolean.parseBoolean(v[12]); 
-                if( b==true){
-                    legendarios.add(cadena);
+            PrintWriter escribir = new  PrintWriter(salida);
+            
+            ArrayList <String> lista = new ArrayList<>();
+            teclado.nextLine();
+           //enlace de escritura
+            
+            while(teclado.hasNextLine()){
+                String cadena = teclado.nextLine(); 
+                String v[] = cadena.split(",");
+                int gen = Integer.parseInt(v[v.length-2]);//toma la penultima 
+                boolean legendary = Boolean.parseBoolean(v[v.length-1]); //toma la ultima
+                if(!legendary && gen==3){
+                    lista.add(cadena);
+                    System.out.println(v[1]+"-"+gen+"-"+legendary); 
+                    escribir.println(v[1]+"-"+gen+"- "+legendary);
                 }
-                   
-            } 
-            System.out.println("Pokemon legendarios: "+legendarios.size()); 
-            for (String legendario : legendarios) {
-                System.out.println(legendario);
             }
             
-
-        }catch( Exception e){
-            System.out.println("Error : "+e.getMessage());
-        }
-        
+            escribir.close();
+            
+        }catch(Exception e ){
+            e.printStackTrace();
+        } 
         
         
     } 
-    // añadir en una lista los pokemon con nombre terminado en vocal
-    // añadir en una lsita los pokemon con total >300
-    // añadir en una lista los pokeon de tipo fuego 
-    //añadir en una lista los pokemones no legendarios de generacion 3
+    
+    public static void terminadoVocal(){
+        try {
+            File archivo = new File("C:\\Users\\usuario\\Desktop\\Primero 24-25\\PROGRAMACION\\generacionNolegendaria.csv"); 
+            File salida = new File("C:\\Users\\usuario\\Desktop\\Primero 24-25\\PROGRAMACION\\generacionNolegendariaTerminadosenVocal.csv"); 
+            PrintWriter escribir = new PrintWriter(salida);
+            Scanner leer = new Scanner(archivo);
+             
+           ArrayList <String> vocal = new ArrayList<>(); 
+           
+            while (leer.hasNextLine()) {                
+                String cadena = leer.nextLine(); 
+                String v[] = cadena.split("-"); 
+                String nombre = v[0];
+                char ultimaLetra = nombre.charAt(nombre.length()-1); 
+                
+                if (ultimaLetra=='a'||ultimaLetra=='e'||ultimaLetra=='i'||ultimaLetra=='o'||ultimaLetra=='u') {
+                    
+                    vocal.add(cadena);
+                    System.out.println(cadena); 
+                    escribir.println(v[0]);
+                }
+            } 
+            escribir.close();
+        
+        
+        } catch (Exception e) { 
+            System.out.println("Error "+e);
+        }
+    }
+    
 }
